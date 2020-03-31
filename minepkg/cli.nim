f866539e3116204d779ebf4c1dd1c32fdaf70afc
Mark Spanbroek <mark@spanbroek.net>
2020-03-31 14:38:10 +0200

    Add command line interface for displaying passwords

--- minepkg/cli.nim
+++ minepkg/cli.nim
@@ -3,17 +3,25 @@ import commandeer
 import ./cli/executable
 import ./cli/create
 import ./cli/delete
+import ./cli/password
 
 commandline:
   subcommand createCommand, "create":
     discard
   subcommand deleteCommand, "delete":
     discard
+  subcommand passwordCommand, "password":
+    argument username, string
+    argument hostname, string
+    errormsg:
+      fmt"Usage: {getExecutableName()} password <username> <hostname>"
 
 proc main*() =
   if createCommand:
     create()
   elif deleteCommand:
     delete()
+  elif passwordCommand:
+    password(username, hostname)
   else:
-    echo fmt"usage: {getExecutableName()} [create|delete]"
+    echo fmt"Usage: {getExecutableName()} [create|delete|password]"
---
+++ minepkg/cli/password.nim
@@ -0,0 +1,40 @@
+import strformat
+import options
+import terminal
+import ../storage
+import ../secrets
+import ../passwords
+import ../displaying
+import ./executable
+
+proc displayErrorNoMainSecret =
+  stderr.writeLine:
+    "No main secret found. Please create one first using " &
+    fmt"'{getExecutableName()} create'"
+
+proc getMainSecret: Secret =
+  let main = retrieveSecret("main")
+  if main.isSome:
+    result = main.get()
+  else:
+    displayErrorNoMainSecret()
+    quit(QuitFailure)
+
+proc displayPassword(username, hostname, password: string) =
+  echo:
+    fmt"Displaying password for {username} at {hostname}. " &
+    "Press a key to continue.\n"
+  let clear = display(password)
+  discard getCh()
+  clear()
+  echo "***-***-***-***"
+
+proc password*(username, hostname: string) =
+  let main = getMainSecret()
+  let passwords = deriveSecret(main, "passwords|0")
+  wipe(main)
+  let password = deriveSecret(passwords,
+      fmt"{username}@{hostname}|0").toPassword
+  wipe(passwords)
+  displayPassword(username, hostname, password)
+  wipe(password)
