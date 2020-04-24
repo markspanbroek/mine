0befc7491f0765fecf7cfec7cf3605df69a5380f
Mark Spanbroek <mark@spanbroek.net>
2020-04-24 17:37:25 +0200

    Run console tests only on POSIX systems
    They don't work on Windows

--- tests/mine/testConsole.nim
+++ tests/mine/testConsole.nim
@@ -6,49 +6,51 @@ import ../helpers/stdout
 import ../helpers/stdin
 import minepkg/console
 
-suite "console":
+if defined(posix):
 
-  const ansiEraseLine = &"\e[2K\e[1G"
-  const ansiCursorUp = &"\e[1A"
-  const ansiErasePreviousLine = ansiCursorUp & ansiEraseLine
+  suite "console":
 
-  let secret = "some secret"
+    const ansiEraseLine = &"\e[2K\e[1G"
+    const ansiCursorUp = &"\e[1A"
+    const ansiErasePreviousLine = ansiCursorUp & ansiEraseLine
 
-  test "displays a secret":
-    stdout.redirect:
-      discard display(secret)
-      check redirected.readAll() == secret & "\n"
+    let secret = "some secret"
 
-  test "clears line after displaying a secret":
-    stdout.redirect:
-      let clear = display(secret)
-      clear()
-      check redirected.readAll() == secret & "\n" & ansiErasePreviousLine
-
-  test "clears multiple lines after displaying a secret":
-    let threeLines = "123".repeat(terminalWidth())
-    stdout.redirect:
-      let clear = display(threeLines)
-      clear()
-      check redirected.readAll() ==
-        threeLines & "\n" & ansiErasePreviousLine.repeat(3)
-
-  test "reads a secret":
-    stdin.redirect:
-      redirected.writeLine(secret)
-      check readSecret() == secret
-
-  test "clears line after reading a secret":
-    stdin.redirect:
-      redirected.writeLine(secret)
+    test "displays a secret":
       stdout.redirect:
-        discard readSecret()
-        check redirected.readAll() == ansiErasePreviousLine
+        discard display(secret)
+        check redirected.readAll() == secret & "\n"
 
-  test "clears multiple lines after reading a secret":
-    let threeLines = "123".repeat(terminalWidth())
-    stdin.redirect:
-      redirected.writeLine(threeLines)
+    test "clears line after displaying a secret":
       stdout.redirect:
-        discard readSecret()
-        check redirected.readAll() == ansiErasePreviousLine.repeat(3)
+        let clear = display(secret)
+        clear()
+        check redirected.readAll() == secret & "\n" & ansiErasePreviousLine
+
+    test "clears multiple lines after displaying a secret":
+      let threeLines = "123".repeat(terminalWidth())
+      stdout.redirect:
+        let clear = display(threeLines)
+        clear()
+        check redirected.readAll() ==
+          threeLines & "\n" & ansiErasePreviousLine.repeat(3)
+
+    test "reads a secret":
+      stdin.redirect:
+        redirected.writeLine(secret)
+        check readSecret() == secret
+
+    test "clears line after reading a secret":
+      stdin.redirect:
+        redirected.writeLine(secret)
+        stdout.redirect:
+          discard readSecret()
+          check redirected.readAll() == ansiErasePreviousLine
+
+    test "clears multiple lines after reading a secret":
+      let threeLines = "123".repeat(terminalWidth())
+      stdin.redirect:
+        redirected.writeLine(threeLines)
+        stdout.redirect:
+          discard readSecret()
+          check redirected.readAll() == ansiErasePreviousLine.repeat(3)
