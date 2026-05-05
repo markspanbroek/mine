45c335f046f92a4c494ecbc5a94fd8cfdd5dca3d
Mark Spanbroek <mark@spanbroek.net>
2026-05-05 11:16:45 +0200

    Remove depencency on regex library
    reason: std/re relies on libpcre, which is no longer
    available on newer linux distros

--- minepkg/passwords.nim
+++ minepkg/passwords.nim
@@ -1,13 +1,17 @@
-import strutils
-import base64
-import re
+import std/strutils
+import std/base64
 import ./secrets
 import ./strings
 
 export wipe
 
 proc toPassword*(secret: Secret): string =
-  result = base64.encode(secret)
-  result = result.replace(re"[^0-9A-Za-z]")
-  result = result[0..<12]
-  result = result.findAll(re".{3}").join("-")
+  var password: string = newStringOfCap(15)
+  for character in base64.encode(secret):
+    if password.len == 15:
+      break
+    if password.len in {3, 7, 11}:
+      password &= "-"
+    if character in Letters + Digits:
+      password &= character
+  password
--- tests/mine/testPasswords.nim
+++ tests/mine/testPasswords.nim
@@ -1,19 +1,23 @@
-import unittest
-import strutils
-import base64
-import re
-import sequtils
+import std/unittest
+import std/strutils
+import std/base64
+import std/sequtils
 import mine
 
 suite "passwords":
 
   test "derives password from secret":
     let secret = createRootKey().deriveSecret("secret")
-    let expected = encode(secret)
-      .replace(re"[^0-9A-Za-z]")
-      .substr(0, 11)
-      .findAll(re".{3}")
-      .join("-")
+    let encoded = encode(secret)
+    let cleaned = encoded.filterIt(it in Letters or it in Digits)
+    let truncated = cleaned[0..<12]
+    let groups = truncated.distribute(4)
+    var expected: string
+    for group in groups:
+      if expected.len > 0:
+        expected &= "-"
+      for character in group:
+        expected &= character
     check secret.toPassword == expected
 
   test "wipes a password":
